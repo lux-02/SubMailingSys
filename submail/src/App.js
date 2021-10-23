2afca1b217ea43e6b4d9a744774f62ecec602f96
오윤석 <oys0608@naver.com>
2021-10-23 14:46:23 +0000

    first

---
+++ submail/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <div className="wrap">
+                <header>
+                    <h1>MSS</h1>
+                    <i>Mailing Service Subscribe</i>
+                </header>
+                <div className="itemWrap">
+                    <div className="item">1</div>
+                    <div className="item">1</div>
+                    <div className="item">1</div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
